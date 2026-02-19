91435db547922d5b45a4a69c1d8b964c6ea6347d
Ashwanth Kumar <Ashwanth.Kumar@valuehealthai.com>
2026-02-19 19:08:53 +0530

    created teh add function with all the things

--- start.py
+++ start.py
@@ -1,21 +1,44 @@
 import json
+import os
+import datetime
+filename = "todo.json"
 
-task_storage = {"name" : ""}
 
 
 while(True):
     get_task_item_input = int(input(" === Todo List Application === \n 1. Add Todo \n 2. View All Todos \n 3. Mark Todo as Complete \n 4. Delete Todo \n 5. Exit \n Enter your choice : "))
+    if(os.path.exists(filename)):
+        with open(filename , "r") as file:
+            data=json.load(file)
+    else:
+        data= []
     if get_task_item_input == 1:
         get_task_item = input("Enter Task Description : ")
-        task_storage["name"] = get_task_item;
-        print(task_storage)
+        storing_dict = {
+            "id" : 0 , 
+            "task": "",
+            "status" : "",
+            "created_at" : ""
+        }
+        data_length = len(data)
+        if data_length==0:
+            last_item_in_list = 0
+        else:    
+            last_item_in_list = data[-1]["id"]
+        storing_dict["id"] = last_item_in_list + 1  
+        storing_dict["task"] = get_task_item
+        storing_dict["status"] = "pending"
+        created_at = datetime.datetime.now()
+        converted_data_time = created_at.strftime("%d/%m/%Y %H:%M:%S")
+        storing_dict["created_at"] = converted_data_time
+        data.append(storing_dict)
+        print(f"To do added suucessfully! ID : {storing_dict["id"]})" )
         with open('todo.json', 'w') as json_file:
-            json.dump(task_storage, json_file)
-    elif get_task_item_input == 5:
+            json.dump(data, json_file)
+    elif get_task_item_input == 5:  
         exit()
     else:
-        print("let's stop this here \n ")
-    
+        print("let's stop this here ")
 
 
 
